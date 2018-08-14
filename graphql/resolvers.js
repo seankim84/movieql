604d3f80fba6c47da19313bc002fc13f7c139534
SeanKim <sean.kim84@gmail.com>
2018-08-14 12:29:42 +0700

    Wrapping a REST API with GraphQL Part 1

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,14 +1,8 @@
-//데이터의 형식 정의하여 함수형식으로 내보내준다.
-import { getMovies, getById, addMovie, deleteMovie } from './db';    
+import { getMovies } from "./db";
 
 const resolvers = {
-    Query: {
-        movies: () => getMovies(),
-        movie: (_, {id}) => getById(id)
-    },
-    Mutation: {
-    addMovie: (_, { name, score }) => addMovie(name, score),    
-    deleteMovie: (_, {id}) => deleteMovie(id)
+  Query: {
+    movies: () => getMovies()
   }
 };
 
